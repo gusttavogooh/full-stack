51543d88e9e58e3ea027d61d5b9ac67134cd84e4
Gustavo Henrique Ferreira <gustavohenriqueferreira110@gmail.com>
2024-02-22 11:31:44 -0300

    Update Styled Components/ Global Style

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
-import './App.css'
-import Header from './components/Header';
+import Header from './components/Header'
+import styled from 'styled-components'
+
+const AppContiner = styled.div`
+  width: 100vw;
+  height: 100vh;
+  background-image: linear-gradient(90deg, #002f52 35%, #326589);
+`
 
 function App() {
   return (
-    <div className="App">
+    <AppContiner>
       <Header />
-    </div>
+    </AppContiner>
   );
 }
 
