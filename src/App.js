8003436f8622a50799331f30a2d9a1d8250a17f0
Gustavo Henrique Ferreira <gustavohenriqueferreira110@gmail.com>
2024-02-22 12:21:00 -0300

    Search component / onBlur

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import Header from './components/Header'
 import styled from 'styled-components'
+import Search from './components/Search';
 
 const AppContiner = styled.div`
   width: 100vw;
@@ -11,6 +12,7 @@ function App() {
   return (
     <AppContiner>
       <Header />
+      <Search />
     </AppContiner>
   );
 }
